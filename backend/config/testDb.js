4979ba5f859ae85cea94b1048e7344968f324055
Manish Kumar <OfficialManish8@outlook.com>
2025-03-28 12:16:41 +0530

    Add backend/config/testDb.js 1743144436664

---
+++ backend/config/testDb.js
@@ -0,0 +1,32 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongoServer;
+
+const connect = async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+};
+
+const disconnect = async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+};
+
+const clearDatabase = async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+};
+
+module.exports = {
+  connect,
+  disconnect,
+  clearDatabase
+};
